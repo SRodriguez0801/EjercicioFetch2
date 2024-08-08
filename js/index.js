b699e64b5e1a63f74e038fbd26705791449345d4
SRodriguez0801 <131110417+SRodriguez0801@users.noreply.github.com>
2024-08-07 21:51:50 -0600

    correcion final

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@ fetch("https://api.escuelajs.co/api/v1/categories")
 .then(data => {
 
     let card = '<div class="row row-cols-1 row-cols-md-3 g-4">'
+
     for(let name of data){
-        card += ` <div class="col">
+        card += `<div class="col">
                    <div class="card">
                       <img src="https://motormotosrr.wordpress.com/wp-content/uploads/2014/06/2013-yamaha-yz450f-right-side-view-04.jpg"
                       class="card-img-top" alt="name">
